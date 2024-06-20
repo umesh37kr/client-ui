3bfc4a34853da5a9d9ee989bace86f7ae84a0559
umesh <umesh37kr@gmail.com>
2024-06-20 10:35:21 +0530

    install shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
       <h1>Welcome to Pizza Shop</h1>
+      <Button>Login</Button>
     </main>
   );
 }
