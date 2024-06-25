5444f5c5e37bbe2b5abe5b27a58daa24377a6f1c
umesh <umesh37kr@gmail.com>
2024-06-26 00:00:59 +0530

    add attributes radio group

--- src/app/(home)/components/ProductCard.tsx
+++ src/app/(home)/components/ProductCard.tsx
@@ -9,6 +9,16 @@ import {
 } from "@/components/ui/card";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 export type Product = {
   id: string;
@@ -39,9 +49,114 @@ const ProductCard = ({ product }: propsType) => {
           <span>From</span>
           <span className="font-bold"> ₹{product.price}</span>
         </p>
-        <Button className="bg-orange-200 hover:bg-orange-300 text-orange-500 px-6 py-2 rounded-full shadow hover:shadow-lg outline-none ease-linear transition-all duration-150">
-          Choose
-        </Button>
+        <Dialog>
+          <DialogTrigger className="bg-orange-200 hover:bg-orange-300 text-orange-500 px-6 py-2 rounded-full shadow hover:shadow-lg outline-none ease-linear transition-all duration-150">
+            Choose
+          </DialogTrigger>
+          <DialogContent className="max-w-3xl p-0">
+            <div className="flex">
+              <div className="w-1/3 bg-white rounded p-8 items-center justify-center">
+                <Image
+                  src={product.image}
+                  width={450}
+                  height={450}
+                  alt="Picture of the author"
+                />
+              </div>
+              <div className="container w-2/3">
+                <h3 className="text-xl font-bold">{product.name}</h3>
+                <p className="mt-1">{product.description}</p>
+                <div>
+                  <h4 className="mt-4">Choose the size</h4>
+                  <RadioGroup
+                    defaultValue="small"
+                    className="grid grid-cols-3 gap-4 mt-2"
+                  >
+                    <div>
+                      <RadioGroupItem
+                        value="small"
+                        id="small"
+                        aria-label="small"
+                        className="peer sr-only"
+                      />
+                      <Label
+                        htmlFor="small"
+                        className="flex flex-col items-center justify-between rounded-md bg-white border-2 p-3 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                      >
+                        Small
+                      </Label>
+                    </div>
+                    <div>
+                      <RadioGroupItem
+                        value="medium"
+                        id="medium"
+                        aria-label="medium"
+                        className="peer sr-only"
+                      />
+                      <Label
+                        htmlFor="medium"
+                        className="flex flex-col items-center justify-between rounded-md bg-white border-2 p-3 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                      >
+                        Medium
+                      </Label>
+                    </div>
+                    <div>
+                      <RadioGroupItem
+                        value="large"
+                        id="large"
+                        aria-label="large"
+                        className="peer sr-only"
+                      />
+                      <Label
+                        htmlFor="large"
+                        className="flex flex-col items-center justify-between rounded-md bg-white border-2 p-3 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                      >
+                        Large
+                      </Label>
+                    </div>
+                  </RadioGroup>
+                </div>
+
+                <div>
+                  <h4 className="mt-4">Choose the size</h4>
+                  <RadioGroup
+                    defaultValue="small"
+                    className="grid grid-cols-3 gap-4 mt-2"
+                  >
+                    <div>
+                      <RadioGroupItem
+                        value="thin"
+                        id="thin"
+                        aria-label="Thin"
+                        className="peer sr-only"
+                      />
+                      <Label
+                        htmlFor="thin"
+                        className="flex flex-col items-center justify-between rounded-md bg-white border-2 p-3 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                      >
+                        Thin
+                      </Label>
+                    </div>
+                    <div>
+                      <RadioGroupItem
+                        value="thick"
+                        id="thick"
+                        aria-label="Thick"
+                        className="peer sr-only"
+                      />
+                      <Label
+                        htmlFor="thick"
+                        className="flex flex-col items-center justify-between rounded-md bg-white border-2 p-3 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                      >
+                        Thick
+                      </Label>
+                    </div>
+                  </RadioGroup>
+                </div>
+              </div>
+            </div>
+          </DialogContent>
+        </Dialog>
       </CardFooter>
     </Card>
   );
